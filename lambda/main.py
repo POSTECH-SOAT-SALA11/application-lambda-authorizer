b075f23dc0612a71df70dfb84b53c69c67d7a72b
Raul Souza <rm355416@fiap.com.br>
2024-08-26 20:40:14 -0300

    first commit

---
+++ lambda/main.py
@@ -0,0 +1,21 @@
+import json
+
+def handler(event, context):
+    """
+    Lambda function handler that returns a simple 'Hello, World!' message.
+
+    Parameters:
+    event (dict): AWS Lambda uses this parameter to pass in event data to the handler.
+    context (object): AWS Lambda uses this parameter to provide runtime information to your handler.
+
+    Returns:
+    dict: A dictionary with a status code and a body containing the 'Hello, World!' message.
+    """
+    response = {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Hello, World!"
+        })
+    }
+
+    return response
