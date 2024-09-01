7e4899301f308bda04e204d733fbc2acd5e78c95
Raul Souza <rm355416@fiap.com.br>
2024-08-31 22:45:55 -0300

    Alterando codigo

--- lambda/main.py
+++ lambda/main.py
@@ -1,21 +1,41 @@
 import json
+import boto3
+from botocore.exceptions import ClientError
 
-def handler(event, context):
-    """
-    Lambda function handler that returns a simple 'Hello, World!' message.
+cognito_client = boto3.client('cognito-idp')
 
-    Parameters:
-    event (dict): AWS Lambda uses this parameter to pass in event data to the handler.
-    context (object): AWS Lambda uses this parameter to provide runtime information to your handler.
+USER_POOL_ID = 'sa-east-1_GSR1Kl3jx'
+APP_CLIENT_ID = '28qfrp9p1t9n5d27dr1o3i1e5e'
 
-    Returns:
-    dict: A dictionary with a status code and a body containing the 'Hello, World!' message.
-    """
-    response = {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "Hello, World!"aaaa
-        })
-    }
+def lambda_handler(event, context):
+    cpf = event['queryStringParameters'].get('cpf')
 
-    return response
+    if not cpf:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('CPF is required')
+        }
+
+    try:
+        response = cognito_client.list_users(
+            UserPoolId=USER_POOL_ID,
+            Filter=f'custom:cpf="{cpf}"'
+        )
+
+        if len(response['Users']) == 0:
+            return {
+                'statusCode': 401,
+                'body': json.dumps('User not found')
+            }
+
+        user = response['Users'][0]
+        return {
+            'statusCode': 200,
+            'body': json.dumps(f'User authenticated: {user["Username"]}')
+        }
+
+    except ClientError as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f'Error authenticating user: {str(e)}')
+        }
